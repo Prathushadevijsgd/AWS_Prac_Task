0b31b79e666e0f8e361fec2d740105d9a060a820
Prathushadevijsgd <jprathushadevi@griddynamics.com>
2025-01-03 17:23:01 +0530

    runinst.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Variables
-INSTANCE_IP="18.204.87.210"  
-KEY_NAME="clikp.pem" 
+INSTANCE_IP="54.174.185.239" 
+KEY_NAME="newkp.pem"  
 ECR_REPOSITORY="my-repository"  
 DOCKER_IMAGE_TAG="latest"  
-AWS_ACCOUNT_ID="767398032219" 
-REGION="us-east-1" 
-HOST_PORT="9090"  
+AWS_ACCOUNT_ID="767398032219"  
+REGION="us-east-1"  
+HOST_PORT="8080"  
 CONTAINER_PORT="8080"  
 
+# Step 1: SSH into EC2 and install Docker
 echo "Installing Docker on the EC2 instance..."
 ssh -o StrictHostKeyChecking=no -i $KEY_NAME ec2-user@$INSTANCE_IP <<EOF
   sudo yum update -y
@@ -18,15 +19,19 @@ ssh -o StrictHostKeyChecking=no -i $KEY_NAME ec2-user@$INSTANCE_IP <<EOF
   sudo usermod -a -G docker ec2-user
   sudo chkconfig docker on
 
+  # Step 2: Authenticate Docker with AWS ECR
   echo "Authenticating Docker with AWS ECR..."
   aws ecr get-login-password --region $REGION | sudo docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
+  # Step 3: Pull Docker image from ECR
   echo "Pulling Docker image from ECR..."
   sudo docker pull $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:$DOCKER_IMAGE_TAG
 
+  # Step 4: Run Docker container
   echo "Running Docker container..."
   sudo docker run -d -p $HOST_PORT:$CONTAINER_PORT $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:$DOCKER_IMAGE_TAG
 EOF
 
-echo "access the application at http://$INSTANCE_IP:$HOST_PORT"
+# Step 5: Provide access URL
+echo "You can access the application at http://$INSTANCE_IP:$HOST_PORT"
 
